e54e747847d48dfc95c470ca393b464cc748068c
Cris <cristian.vidal.lopezgmail.com>
2020-07-17 12:29:05 -0400

    // Initial Commit on master, this is the first State of the example fragment app guide 01.

---
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -0,0 +1,15 @@
+package com.crisspian.fragment_guide_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
