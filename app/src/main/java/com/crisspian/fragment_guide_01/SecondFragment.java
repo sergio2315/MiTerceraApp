c213a98af1f056c970d6d88fafa582ddaac341a5
Sergio Lillo <Sergiolillo2315@gmail.com>
2020-12-21 13:19:26 -0300

    Implementando Fragmentos y RadioButton

--- app/src/main/java/com/crisspian/fragment_guide_01/SecondFragment.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/SecondFragment.java
@@ -1,5 +1,7 @@
 package com.crisspian.fragment_guide_01;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,48 +13,52 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 
-import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
-import com.crisspian.fragment_guide_01.databinding.FragmentFirstBinding;
+import com.crisspian.fragment_guide_01.databinding.FragmentSecondBinding;
 
-public class FirstFragment extends Fragment {
-private FragmentFirstBinding binding;
-    public FirstFragment() {
+
+public class SecondFragment extends Fragment {
+private FragmentSecondBinding binding;
+    public SecondFragment() {
         // Required empty public constructor
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentFirstBinding.inflate(inflater, container,false);
+        binding = FragmentSecondBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.RadioGroup1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        binding.RadioGroup2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                int index = binding.RadioGroup1.indexOfChild(binding.RadioGroup1.findViewById(checkedId));
+                int index = binding.RadioGroup2.indexOfChild(binding.RadioGroup2.findViewById(checkedId));
                 switch (index){
                     case 0:
-                        binding.textFragmento1.setText("correcto");
+                        String url = "https://www.youtube.com";
+                        Intent mIntent = new Intent(Intent.ACTION_VIEW);
+                        mIntent.setData(Uri.parse(url));
+                        startActivity(mIntent);
                         break;
                     case 1:
-                        binding.textFragmento1.setText("equivocado");
-                        break;
+
+                        binding.textFragamento2.setText("ok");
                 }
             }
         });
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
-        binding =null;
+        binding = null;
     }
 }
